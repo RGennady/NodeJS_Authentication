fbf3ac0f122b770f64c02423a5d3d06f0d8b4f19
BrewCanningSystem <95075775+BrewCanningSystem@users.noreply.github.com>
2022-03-30 20:19:38 +0300

    версия 1.0.2
    изменены тексты ответов

--- index.js
+++ index.js
@@ -33,16 +33,16 @@ router.use(function(req,res,next) {
         })
       }
       catch(err){
-         res.status(403).json({message: 'Токен недействителен', status:403})
+         res.status(403).json({'status':403, 'message': 'Токен недействителен'})
        }
     }
     else{
-         res.status(401).json({message: 'Пожалуйста, отправьте токен ', status:401});
+         res.status(401).json({'status':401,'message': 'Пожалуйста, отправьте токен'});
     }
   }
   catch(err){
     console.log(err);
-    res.status(400).json({'status':400,'message':'Сервер не понимает запрос из-за неверного синтаксиса', 'result':[]});
+    res.status(400).json({'status':400,'message':'Сервер не понимает запрос из-за неверного синтаксиса'});
  } 
 });
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -52,21 +52,21 @@ router.post('/test', async(req, res) =>{
    connection.query(sql, function (err, result){
      if( err ){
          console.log(err);
-         res.status(400).json({'status':400,'message':'Ошибка БД', 'result':[]});
+         res.status(400).json({'status':400,'message':'Ошибка БД'});
        }
        else{
           if(result.length>0){ 
             res.status(200).json({'status':200,'message':'Выполнено успешно', 'result':result});
           }
           else{
-             res.status(404).json({'status':404,'message':'По запросу ничего не найдено', 'result':[]});
+             res.status(404).json({'status':404,'message':'По запросу ничего не найдено', 'result':-1});
           }
        }
     });
  }
  catch(err){
     console.log(err);
-    res.status(400).json({'status':400,'message':'Сервер не понимает запрос из-за неверного синтаксиса', 'result':[]});
+    res.status(400).json({'status':400,'message':'Сервер не понимает запрос из-за неверного синтаксиса'});
  }
 });
 
